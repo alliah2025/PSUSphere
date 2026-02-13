c9ff365839b22db942288e77b4b6ee95cbd3ec68
unknown <202380059@psu.palawan.edu.ph>
2026-02-13 15:27:04 +0800

    refactor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,23 +1,34 @@
 from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
+
 @admin.register(College)
 class CollegeAdmin(admin.ModelAdmin):
     list_display = ("college_name", "created_at", "updated_at")
     search_fields = ("college_name",)
     list_filter = ("created_at",)
+    
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name",)
+    list_filter = ("college",)
 
-admin.site.register(Program)
-admin.site.register(Organization)
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
 
 @admin.register(Student) 
 class StudentAdmin(admin.ModelAdmin): 
-    list_display = ("student_id", "lastname","firstname", "middlename", "program") 
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program") 
     search_fields = ("lastname", "firstname",) 
+
 
 @admin.register(OrgMember) 
 class OrgMemberAdmin(admin.ModelAdmin): 
-    list_display = ("student", "get_member_program", "organization","date_joined",) 
+    list_display = ("student", "get_member_program", "organization", "date_joined",) 
     search_fields = ("student__lastname", "student__firstname",)
  
     def get_member_program(self, obj): 
@@ -25,4 +36,4 @@ class OrgMemberAdmin(admin.ModelAdmin):
             member = Student.objects.get(id=obj.student_id) 
             return member.program 
         except Student.DoesNotExist: 
-            return None 
+            return None
