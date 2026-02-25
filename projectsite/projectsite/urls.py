e29efdebcc7700891b2e8615159e5a2e29bd6a95
alliah2025 <202380062@psu.palawan.edu.ph>
2026-02-25 14:44:45 +0800

    add login auth and socials. Not final

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg import views
 
@@ -28,6 +28,7 @@ from studentorg.views import (
 
 urlpatterns = [ 
     path("admin/", admin.site.urls), 
+    path("accounts/", include("allauth.urls")), # allauth routes
     path('', views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
